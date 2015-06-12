dd4e7e89bf16d98b814dbfa96b96ab5ccfa3c273
EdvinasGl <edvinasg@gmail.com>
2015-06-12 18:54:17 +0300

    Jokiu pakitimu. Testas

--- src/test/java/CloudTest/NewTest.java
+++ src/test/java/CloudTest/NewTest.java
@@ -258,7 +258,7 @@ public void setUp(Method method) throws Exception {
  
  @Test (priority = 5)
  public void testControl() throws Exception {
-	 Thread.sleep(150000);
+	 Thread.sleep(150000); 
 	 new Control(driver);
 	 Control.execute();
 	
