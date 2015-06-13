522f30b59947410f7930b8bcafe12f579a0878b5
EdvinasGl <edvinasg@gmail.com>
2015-06-13 17:17:32 +0300

    Sukurtas testNg suitas, padidintas waitas

--- src/test/java/CloudTest/NewTest.java
+++ src/test/java/CloudTest/NewTest.java
@@ -258,7 +258,7 @@ public void setUp(Method method) throws Exception {
  
  @Test (priority = 5)
  public void testControl() throws Exception {
-	 Thread.sleep(240000); 
+	 Thread.sleep(270000); 
 	 new Control(driver);
 	 Control.execute();  
 	  
