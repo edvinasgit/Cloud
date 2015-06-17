cad3faf9605a19c377ab993e9892e1e830f4df23
EdvinasGl <edvinasg@gmail.com>
2015-06-17 20:59:14 +0300

    Dar padidintas waitas iki 160s

--- src/test/java/CloudTest/NewTest.java
+++ src/test/java/CloudTest/NewTest.java
@@ -138,7 +138,7 @@ public void testLoginDev() throws Exception {
 		    driver.findElement(By.id("pinSubmit")).click();
 		    Info.info("AddDevice Passed");
 		    System.out.println("Pin Code entered");
-		    WebDriverWait wait2 = new WebDriverWait(driver, 90);
+		    WebDriverWait wait2 = new WebDriverWait(driver, 160);
 		    wait2.until(ExpectedConditions.visibilityOfElementLocated((By.className("device_on"))));
 		    System.out.println("Device Added");
 	    	
